d11c45b23881e2b862c5952ffd27144e76e89061
Miroslav Milivojevic <miroslav.p.milivojevic@gmail.com>
2020-12-01 16:09:07 +0100

    Localized Manager added

--- Sources/LocalZKit/LocalZKit.swift
+++
@@ -1,3 +0,0 @@
-struct LocalZKit {
-    var text = "Hello, World!"
-}
---
+++ Sources/LocalZKit/LocalZManager.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miroslav Milivojevic on 01/12/2020.
+//
+
+import Foundation
+
+class LocalZManager {
+    static let shared: LocalZManager = LocalZManager()
+    init() {}
+    
+    var translates = [String: String]()
+}
+
+
+extension String {
+    func localized() -> String {
+        LocalZManager.shared.translates[self] ?? self
+    }
+    
+    var localized: String {
+        self.localized()
+    }
+}
+
